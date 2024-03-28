29cbd0e59acd4e4c0345ac5410b4aa4e223d240f
BEHAGUE Quentin <quentinbehague59560@gmail.com>
2024-03-28 12:41:25 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function afficher_texte(id){
-    var span = document.getElementById(id);
-    if(span.style.display == "none") {
-      span.style.display = "inline";
-    } else {
-      span.style.display = "none";
-    }
-  }
